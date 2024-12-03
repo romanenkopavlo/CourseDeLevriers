19a97e2ed5e89895fd91a74bfd7aa21a0a4eb545
promanenko <paihitetty444@gmail.com>
2024-12-03 11:30:29 +0100

    Add project files.

---
+++ CourseDeLevriers/Levrier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseDeLevriers
+{
+    public class Levrier
+    {
+        private int instanceNumber;
+
+        public int InstanceNumber
+        {
+            set
+            {
+                this.instanceNumber = value;
+            }
+        }
+        public int Distance { get; set; }
+        public Mutex Mutex { get; set; }
+        public int Color { get; internal set; }
+        public Random Random { get; set; }
+        public ManualResetEvent depart;
+        public ManualResetEvent arrivee;
+
+        public void Run()
+        {
+            depart.WaitOne();
+            for (int i = 0; i < Distance; i++)
+            {
+                Thread.Sleep(this.Random.Next(10));
+                Mutex.WaitOne();
+                Console.ForegroundColor = (ConsoleColor)Color;
+                Console.WriteLine("Le levrier numero " + this.instanceNumber + " parcouru: " + (i + 1) + " metres");
+                Mutex.ReleaseMutex();
+            }
+            arrivee.Set();
+        }
+    }
+}
---
+++ CourseDeLevriers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.IO.IsolatedStorage;
+
+namespace CourseDeLevriers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex();
+            Random random = new Random();
+            ManualResetEvent[] events = new ManualResetEvent[10];
+            ManualResetEvent depart = new ManualResetEvent(false);
+            List<int> ordreArrivee = new List<int>();
+
+            for (int i = 0; i < 10; i++) 
+            {
+                Levrier levrier = new Levrier();
+                levrier.InstanceNumber = i;
+                levrier.Random = random;
+                levrier.Color = i + 1;
+                levrier.Mutex = mutex;
+                levrier.Distance = 50;
+                levrier.arrivee = new ManualResetEvent(false);
+                levrier.depart = depart;
+                events[i] = levrier.arrivee;
+
+                Thread thread = new Thread(levrier.Run);
+                thread.Start();
+            }
+
+            depart.Set();
+
+            int position;
+            int arrives = 0;
+
+            while (arrives < 10)
+            {
+                position = WaitHandle.WaitAny(events);
+                mutex.WaitOne();
+                Console.WriteLine("Le n° " + position + " est arrivé !   <=======================================================================");
+                mutex.ReleaseMutex();
+                arrives++;
+                ordreArrivee.Add(position);
+                events[position].Reset();
+            }
+
+            Console.WriteLine("-----===== Tableau des arrivées =====-----");
+            foreach (int i in ordreArrivee)
+            {
+                Console.WriteLine("Levrier " + i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
